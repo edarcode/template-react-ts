c760617ea7fd107ce6d38f0702f5bb9f1ea150cf
Edwin Ortiz <edarcode@gmail.com>
2024-09-27 04:42:12 -0500

    agg hook para realizar fetch

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,3 @@
-import { EdarErr } from "../errors/EdarErr";
-import { Fetch, useFetch } from "../hooks/useFetch";
-
 export default function App() {
-  const { data, loading, err } = useFetch<object, null>(service);
-  console.log(data);
-  if (loading) return <>Cargando...</>;
-  if (err) return <>{err.msg}</>;
   return <>App</>;
 }
-
-const service: Fetch<object, null> = async ({ signal }) => {
-  const res = await fetch("https://kitsu.io/api/edge/anime", { signal });
-
-  if (!res.ok) throw new EdarErr({ status: 400, msg: "Algo salió mal" });
-  return await res.json();
-};
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,31 +1,49 @@
 import { useEffect, useState } from "react";
 import { EdarErr } from "../errors/EdarErr";
 
-export const useFetch = <Data, Payload>(fetch: Fetch<Data, Payload>) => {
-  const [data, setData] = useState<Data>();
-  const [loading, setLoading] = useState(false);
-  const [err, setErr] = useState<EdarErr>();
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    fetch({ signal: controller.signal })
-      .then((data) => setData(data))
-      .catch((err: EdarErr) => setErr(err))
-      .finally(() => setLoading(false));
-
-    return () => controller.abort();
-  }, [fetch]);
-
-  return { data, loading, err };
+const initFetchState = {
+  err: undefined,
+  loading: false,
+  payload: undefined,
+  data: undefined,
 };
 
-export type Fetch<Data, Payload> = (
+export const useFetch = <Payload, Data>(fetch: Fetch<Payload, Data>) => {
+  const [fetchState, setFetchState] =
+    useState<FetchState<Payload, Data>>(initFetchState);
+
+  const { err, loading, payload, data } = fetchState;
+
+  useEffect(() => {
+    if (!loading) return;
+    const controller = new AbortController();
+
+    fetch({ signal: controller.signal, payload })
+      .then((data) => setFetchState({ ...initFetchState, data }))
+      .catch((err: EdarErr) => setFetchState({ ...initFetchState, err }));
+
+    return () => controller.abort();
+  }, [fetch, loading, payload]);
+
+  const startFetch = (payload?: Payload) => {
+    setFetchState({ ...initFetchState, payload, loading: true });
+  };
+
+  return { data, loading, err, startFetch };
+};
+
+export type Fetch<Payload, Data> = (
   params: FetchParams<Payload>
 ) => Promise<Data>;
 
 type FetchParams<Payload> = {
   signal: AbortSignal;
   payload?: Payload;
+};
+
+type FetchState<Payload, Data> = {
+  err: EdarErr | undefined;
+  loading: boolean;
+  payload: Payload | undefined;
+  data: Data | undefined;
 };
